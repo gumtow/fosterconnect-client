2212e88f7ac830043f76033c1b2153a14fd8435f
jessegumtow <gumtow@gmail.com>
2020-08-02 13:36:09 -0500

    simplified child creation form

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -47,55 +47,6 @@ export default class Dashboard extends Component {
         });
     };
 
-    // On file select
-    onFileChange = event => {
-        event.persist()
-        this.setState(()=>{
-            return{
-                [event.target.name]: event.target.files[0]
-            }
-        })
-    };
-
-    // file upload button clicked
-    onFileUpload = () => {
-        const formData = new FormData();
-
-        formData.append("image", this.state.image)
-        // formData.append("video", this.state.video)
-        // console.log(this.state.selectedFile);
-
-        axios.post("https://fosterconnect-api.herokuapp.com/items", formData, {withCredentials: true})
-        .then( response =>{
-            console.log(response);
-        }).catch(error => {
-            console.log("ITEM ERROR", error);
-        });
-    };
-
-    // File content to be displayed after file upload is complete
-    // fileData = () => {
-    //     if (this.state.selectedFile){
-    //         return(
-    //             <div> 
-    //         <h2>File Details:</h2> 
-    //         <p>File Name: {this.state.selectedFile.name}</p> 
-    //         <p>File Type: {this.state.selectedFile.type}</p> 
-    //         <p> 
-    //           Last Modified:{" "} 
-    //           {this.state.selectedFile.lastModifiedDate.toDateString()} 
-    //         </p> 
-    //       </div> 
-    //         );
-    //     } else {
-    //         return ( 
-    //             <div> 
-    //               <br /> 
-    //               <h4>Choose before Pressing the Upload button</h4> 
-    //             </div> 
-    //           ); 
-    //     }
-    // };
 
     
 
@@ -155,12 +106,6 @@ export default class Dashboard extends Component {
                     <form onSubmit={this.handleSubmit} >
                         <input type="string" name="name" placeholder="Child's name" value={this.state.name} onChange={this.handleChange} required />
                         <input type="string" name="status" placeholder={this.state.status} value={this.state.status} onChange={this.handleChange} required />
-                        <input type="file" name="image" placeholder="Upload an image" value={this.state.pictures} onChange={this.onFileChange} required />
-                        <button onClick={this.onFileUpload}>Upload</button>
-                        {/* <div>
-                            {this.fileData()}
-                        </div> */}
-                        <input type="string" name="documents" placeholder="document url" value={this.state.documents} onChange={this.handleChange} required />
                         <button type="submit">Add Child</button>
                     </form>
                     <div>
